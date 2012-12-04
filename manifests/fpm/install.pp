211fe7a3e1343dc958f630ac69d4d78994f97fee
Martin Loy <martin@guruhub.com.uy>
2012-12-04 14:22:10 -0200

    probando nuevo parameter linkeado a guruhub/puppet-php/#1

--- manifests/fpm/install.pp
+++ manifests/fpm/install.pp
@@ -1,4 +1,4 @@
-class php::fpm::install(fpm_package_version => $php::params::fpm_package_version, ) { 
+class php::fpm::install(fpm_package_version = $php::params::fpm_package_version, ) { 
     package { $php::params::fpm_package_name:
         ensure  => $fpm_package_version,
         require => Class["php"],
